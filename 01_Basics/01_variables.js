a5a0591cb40f4e65014cc352f6753f895131fdae
Kumar Mridul <135414241+Mridul-Srivastava03@users.noreply.github.com>
2025-06-22 03:13:33 +0000

    Learning in Progress

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1445553
+let accountEmail = "1234@Google.com"
+var accountPassword = "123445"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+accountEmail = "hi@google.com"
+accountPassword = "235"
+accountCity = "Patna"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// alert(3+3)
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let temp = null
+let course
+console.log(typeof(age));
+console.table([typeof name, typeof(age), typeof(isLoggedIn), typeof(temp), typeof(course)]);
+console.log(typeof null);
+console.log(typeof(null))
+console.log(typeof(undefined));
+
